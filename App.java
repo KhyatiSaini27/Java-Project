ef6fbeebaae6edd686606fc54a51cffaf526a7a3
KhyatiSaini27 <ks2339@srmist.edu.in>
2024-10-24 17:01:43 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+package TicTacToe;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        TicTacToe ticTacToe = new TicTacToe();
+    }
+}
